38034e8c8e0a7af826b2789bd6ab21d8bf32ae3c
lasindusenash05 <lasindusenash05@gmail.com>
2025-06-16 15:45:08 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-
-import '../styles/theme.css'
+import '../styles/Home.module.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Sinhala&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
