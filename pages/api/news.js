0e398398cdc75bf63554e7172ce2a315c5ff6142
lasindusenash05 <lasindusenash05@gmail.com>
2025-06-16 15:44:07 +0530

    Update news.js

--- pages/api/news.js
+++ pages/api/news.js
@@ -1,24 +1,25 @@
-
-import axios from 'axios'
-import * as cheerio from 'cheerio'
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
 export default async function handler(req, res) {
   try {
-    const { data } = await axios.get('https://sinhala.adaderana.lk/')
-    const $ = cheerio.load(data)
-    const articles = []
+    const { data } = await axios.get('https://www.hirunews.lk/sinhala');
+    const $ = cheerio.load(data);
 
-    $('.lead-news-block .media').each((i, el) => {
-      const title = $(el).find('.media-body > h2').text().trim()
-      const image = $(el).find('img').attr('src')
-      const link = 'https://sinhala.adaderana.lk' + $(el).find('a').attr('href')
+    const news = [];
+
+    $('.other_news_list .news_box').each((i, el) => {
+      const title = $(el).find('a h2').text().trim();
+      const image = $(el).find('a .image img').attr('src');
+      const link = 'https://www.hirunews.lk' + $(el).find('a').attr('href');
+
       if (title && image && link) {
-        articles.push({ title, image, link })
+        news.push({ title, image, link });
       }
-    })
+    });
 
-    res.status(200).json(articles)
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch news' })
+    res.status(200).json(news.slice(0, 10)); // Return top 10 news items
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch news' });
   }
 }
